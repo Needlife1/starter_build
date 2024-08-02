92e0d809a0522392c83046a5c14e180da40d328f
Visotsky Maxim <needlife124@gmail.com>
2024-08-02 17:20:18 +0300

    don

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,5 +1,11 @@
 import autoprefixer from 'autoprefixer';
+import purgecss from '@fullhuman/postcss-purgecss';
+
+const purgeCssPlugin = purgecss({
+  content: ['./src/**/*.{html,ts,js,jsx,tsx}'],
+  safelist: ['safe-class'], // если есть классы, которые не должны быть удалены
+});
 
 export default {
-  plugins: [autoprefixer()],
+  plugins: [purgeCssPlugin, autoprefixer()],
 };
